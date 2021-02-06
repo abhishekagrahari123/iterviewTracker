e2b41fb3775b34c195cd0323d02762e0d302a0c7
Abhishek Agrahari <abhishekagrahari2002@gmail.com>
2021-02-07 00:47:28 +0530

    photo work

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -12,6 +12,8 @@ const viewquestion = async (req,res) =>
 {
   const id= req.params.id;
   l1 = await question.find({topic:id});
+  l2 = await topic.findById(id);
+  res.locals.popo = l2;
   const result = await topic.find();
   res.locals.rest = result;
   res.render('main/question.ejs' , {list:l1});
